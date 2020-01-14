c1eab518cbe284bb11df7e311438b0a74e354891
tiro-finale <syubarutusiruto@icloud>
2020-01-14 10:23:54 +0900

    fix?

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from pprint import pprint
 from itertools import product
 import sys
-
-"""
-n=1
-R
-
-n=2
-(R+R) -> (f(1) + f(1))
- R*R  ->  f(1) * f(1)
-
-n=3
-R+R+R   -> f(2) + f(1) or f(1) + f(2)
-(R+R)*R -> f(2) * f(1)
-R*R*R   -> f(2) * f(1) or f(2) * f(1)
-R*R+R   -> f(2) + f(1)
-
-n=4
-  (R*R+R)*R -> (f(3)) * f(1)
-    R*R*R+R -> f(3) + f(1)
-    R*R+R*R -> f(3) * f(1)
-    R*R+R+R -> f(3) + f(1)
-  (R+R)*R*R -> f(3) * f(1)
-(R+R)*(R+R) -> f(2) * f(2)
-  R+R*(R+R) -> f(1) + f(3)
-    R*R*R*R -> f(3) * f(1) or f(1) * f(3)
-  (R+R+R)*R -> (f(3)) * f(1)
-    R+R+R+R -> f(3) + f(1)
-"""
 
 def f(n):
 
@@ -43,30 +18,29 @@ def f(n):
         left, right = f(n-i), f(i)
 
         if type(left) is str and type(right) is str:
-            l.append("%s+%s"%(left, right))
+            #l.append("%s+%s"%(left, right))
             l.append("(%s+%s)"%(left, right))
             l.append("%s*%s"%(left, right))
         else:
-            l.append([left, "+", right])
+            #l.append([left, "+", right])
             l.append(["(", left, "+", right, ")"])
             l.append([left, "*", right])
 
     return l
 
 
-def xprint(data):
-    for d in data:
-        if type(d) is str:
-            print(d, end="")
-        elif type(d) is list:
-            xprint(d)
-
-    print("")
-
 def exe(n):
     ans = f(n)
     for a in ans:
         #print(''.join([str(aa) for aa in a]))
+        """
+        if type(a) is list:
+            print("{", end="")
+            print(*a, end="")
+            print("}")
+        else:
+            print(a)
+        """
         print(a)
 
 if __name__ == "__main__":
